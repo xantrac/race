bff64a9b63494a09b39ac19189dd7c304dcf6c61
Marco Dell'Olio <dellolio.marco@gmail.com>
2020-08-20 11:34:07 -0400

    updated with parallelization

--- bench/test/bench_test.exs
+++ bench/test/bench_test.exs
@@ -1,25 +1,29 @@
 defmodule BenchTest do
   use ExUnit.Case
 
-  def benchmark() do
+  def benchmark(parallel) do
     Benchee.run(
       %{
         "rust" => fn n -> Client.generate_rust_primes(n) end,
         "rust/elixir" => fn n -> Client.generate_elixir_primes(n) end
       },
       warmup: 4,
-      time: 5,
+      time: 10,
       inputs: %{
-        "small" => Enum.random(0..5000),
-        "medium" => Enum.random(10_000..40_000)
+        "small" => Enum.random(0..5000)
       },
-      parallel: 1,
+      parallel: parallel,
       formatters: [
         Benchee.Formatters.Console,
-        {Benchee.Formatters.HTML, file: "output/benchmark.html"}
+        {Benchee.Formatters.HTML, file: "output_parallel_#{parallel}/benchmark_parallel.html"}
       ]
     )
   end
 
-  test "benchmark", do: benchmark()
+  test "benchmark" do
+    benchmark(1)
+    benchmark(4)
+    benchmark(10)
+    benchmark(20)
+  end
 end
